f84d8d36fc5b865ebcaf0cb95b3d3666438ccc26
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-07-06 16:59:53 +0100

    _

---
+++ Http/MediaTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace Sensemaking.Http
+{
+    public class MediaType
+    {
+        public static readonly string Html = "text/html";
+        public static readonly string Json = "application/json";
+        public static readonly string Siren = "application/vnd.siren+json";
+        public static readonly string JsonProblem = "application/problem+json";
+    }
+}
