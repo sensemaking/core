ff1490565b4d86bdc1477f2747c353f461c86dbe
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-07-14 19:48:09 +0100

    _

---
+++ Bdd/ReflectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+
+namespace System
+{
+    public static class ReflectionExtensions
+    {
+        public static T GetReflectedValue<T>(this object source, string propertyName) 
+        {
+            return (T) source.GetType().GetInfo(propertyName)?.GetValue(source, null);  
+        }
+
+        public static void SetReflectedValue(this object source, string propertyName, object value)
+        {
+            source.GetType().GetInfo(propertyName)?.SetValue(source, value);
+        }
+
+        private static PropertyInfo GetInfo(this Type type, string propertyName)
+        {
+            var info = type?.GetProperty(propertyName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy);
+            while (info == null && type != null)
+                info = type.BaseType.GetInfo(propertyName);
+
+            return info;
+        }
+    }
+}
