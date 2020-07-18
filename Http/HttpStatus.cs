48dc779873f3fb7e93a168b1664dfa46168dce9a
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-07-18 08:56:26 +0100

    _

--- Http/HttpStatus.cs
+++ Http/HttpStatus.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace Sensemaking.Http
 {
@@ -12,5 +13,35 @@ namespace Sensemaking.Http
 
         public HttpStatusCode Code { get; }
         public string Reason { get; }
+
+
+        #region Equality
+
+        public static bool operator ==(HttpStatus @this, HttpStatus that)
+        {
+            return @this.Equals(that);
+        }
+
+        public static bool operator !=(HttpStatus @this, HttpStatus that)
+        {
+            return !@this.Equals(that);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is HttpStatus other && Equals(other);
+        }
+
+        public bool Equals(HttpStatus that)
+        {
+            return this.Code == that.Code && this.Reason == that.Reason;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Code, Reason);
+        }
+
+        #endregion
     }
 }
