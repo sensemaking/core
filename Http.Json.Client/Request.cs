a9ada6abf4f2b3d6f62acb4c248fc35aebe29414
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-07-20 12:39:36 +0100

    _

--- Http.Json.Client/Request.cs
+++ Http.Json.Client/Request.cs
@@ -15,27 +15,55 @@ namespace Sensemaking.Http.Json.Client
         public static async Task<JsonResponse<T>> GetAsync<T>(this string url, params (string Name, string Value)[] headers)
         {
             var response = await url.WithHeaders(headers.AddAcceptHeader()).GetAsync();
-            var body = await response.Content.ReadAsStringAsync();
-            return new JsonResponse<T>(body, response);
+            return await response.ToJsonResponse<T>();
         }
 
-        public static async Task<Json.Client.JsonResponse> PutAsync(this string url, object payload, params (string Name, string Value)[] headers)
+        public static async Task<JsonResponse<T>> GetAsync<T>(this FlurlClient client, string url, params (string Name, string Value)[] headers)
+        {
+            var response = await client.Request(url).WithHeaders(headers.AddAcceptHeader()).GetAsync();
+            return await response.ToJsonResponse<T>();
+        }
+
+        public static async Task<JsonResponse> PutAsync(this string url, object payload, params (string Name, string Value)[] headers)
         {
             var response = await url.WithHeaders(headers.AddAcceptHeader()).PutAsync(payload.ToRequestBody());
-            return new Json.Client.JsonResponse(response);
+            return new JsonResponse(response);
         }
 
-        public static async Task<Json.Client.JsonResponse> DeleteAsync(this string url, params (string Name, string Value)[] headers)
+        public static async Task<JsonResponse> PutAsync(this FlurlClient client, string url, object payload, params (string Name, string Value)[] headers)
+        {
+            var response = await client.Request(url).WithHeaders(headers.AddAcceptHeader()).PutAsync(payload.ToRequestBody());
+            return new JsonResponse(response);
+        }
+
+        public static async Task<JsonResponse> DeleteAsync(this string url, params (string Name, string Value)[] headers)
         {
             var response = await url.WithHeaders(headers.AddAcceptHeader()).DeleteAsync();
-            return new Json.Client.JsonResponse(response);
+            return new JsonResponse(response);
         }
 
-        public static async Task<Json.Client.JsonResponse> PostAsync(this string url, object payload, params (string Name, string Value)[] headers)
+        public static async Task<JsonResponse> DeleteAsync(this FlurlClient client, string url, params (string Name, string Value)[] headers)
         {
-            headers.AddAcceptHeader();
+            var response = await client.Request(url).WithHeaders(headers.AddAcceptHeader()).DeleteAsync();
+            return new JsonResponse(response);
+        }
+
+        public static async Task<JsonResponse> PostAsync(this string url, object payload, params (string Name, string Value)[] headers)
+        {
             var response = await url.WithHeaders(headers.AddAcceptHeader()).PostAsync(payload.ToRequestBody());
-            return new Json.Client.JsonResponse(response);
+            return new JsonResponse(response);
+        }
+
+        public static async Task<JsonResponse> PostAsync(this FlurlClient client, string url, object payload, params (string Name, string Value)[] headers)
+        {
+            var response = await client.Request(url).WithHeaders(headers.AddAcceptHeader()).PostAsync(payload.ToRequestBody());
+            return new JsonResponse(response);
+        }
+
+        private static async Task<JsonResponse<T>> ToJsonResponse<T>(this HttpResponseMessage response)
+        {
+            var body = await response.Content.ReadAsStringAsync();
+            return new JsonResponse<T>(body, response);
         }
 
         private static (string, string)[] AddAcceptHeader(this IEnumerable<(string, string)> headers)
@@ -47,5 +75,9 @@ namespace Sensemaking.Http.Json.Client
         {
             return new StringContent(payload.Serialize(), Encoding.UTF8, MediaType.Json);
         }
+    }
+
+    internal static class ResponseExtensions
+    {
     }
 }
