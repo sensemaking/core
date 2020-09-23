696bbe6f63e0de8ef8625ce883c28263d9269e86
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2020-09-23 15:12:46 +0100

    _

--- Core/Language.cs
+++ Core/Language.cs
@@ -1,10 +1,19 @@
-﻿namespace System
+﻿using System.Text.RegularExpressions;
+
+namespace System
 {
     public static class Language
     {
         public static bool IsNullOrEmpty(this string? source)
         {
             return string.IsNullOrWhiteSpace(source);
+        }
+
+        public static bool IsBase64(this string? source)
+        {
+            source = source?.Trim();
+            return  !source.IsNullOrEmpty() && (source!.Length % 4 == 0) && Regex.IsMatch(source, @"^[a-zA-Z0-9\+/]*={0,3}$", RegexOptions.None);
+
         }
     }
 }
