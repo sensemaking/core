965ab5a3c85321f931eacfaa2072af817282ec08
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2022-06-20 11:25:21 +0100

    _

--- Http/MediaTypes.cs
+++ Http/MediaTypes.cs
@@ -7,4 +7,10 @@
         public const string Siren = "application/vnd.siren+json";
         public const string JsonProblem = "application/problem+json";
     }
+
+    public static class Charsets
+    {
+        public const string Utf8 = "charset=UTF-8";
+        public const string Utf16 = "charset=UTF-16";
+    }
 }
