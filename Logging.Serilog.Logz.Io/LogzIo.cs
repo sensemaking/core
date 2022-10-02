51eb2fb8d699b966e45d248ceec01aba9ee64d2f
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2022-10-02 14:27:10 +0100

    _

---
+++ Logging.Serilog.Logz.Io/LogzIo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Serilog;
+using Serilog.Sinks.Logz.Io;
+
+namespace Sensemaking
+{
+    public static class LogzIo
+    {
+        public static ILogger CreateLogger(Settings settings)
+        {
+            return new LoggerConfiguration()
+                .WriteTo.LogzIoEcs(new LogzioEcsOptions
+                {
+                    Type = settings.Type.ToString(),
+                    AuthToken = settings.Token,
+                    DataCenter = new LogzioDataCenter { SubDomain = settings.SubDomain, Port = settings.Port, UseHttps = true }
+                }, restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Verbose)
+                .CreateLogger();
+        }
+
+
+        public record Settings(string SubDomain, int Port, string Token, LogzType Type);
+
+        public enum LogzType
+        {
+            Local = 1,
+            Development = 1,
+            Production = 2
+        }
+    }
+
+}
---
+++ Logging.Serilog/NullLogger.cs
@@ -0,0 +1,9 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace Sensemaking;
+
+public class NullLogger : ILogger
+{
+    public void Write(LogEvent logEvent) { }
+}
