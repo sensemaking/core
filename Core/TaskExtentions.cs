7fd99139472a3b29553b0f3a4672f17df0c2b019
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2023-11-14 17:02:15 +0000

    _

---
+++ Core/TaskExtentions.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace System;
+
+public static class TaskExtentions
+{
+    public static T Await<T>(this Task<T> task) => task.GetAwaiter().GetResult();
+    public static void Await(this Task task) => task.GetAwaiter().GetResult();
+}
