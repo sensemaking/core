c405b0d850cd8cdef871a880947331e70caee55d
eldon@sensemaking.uk.com <eldon@sensemaking.uk.com>
2025-09-18 14:03:07 +0100

    _

--- Bdd/LoggingAssertions.cs
+++ Bdd/LoggingAssertions.cs
@@ -46,5 +46,45 @@ namespace Sensemaking.Bdd
         {
             logger.Received().Fatal(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Alert, entry).Serialize());
         }
+        
+        public static void should_not_have_logged_as_information(this ILogger logger, object entry)
+        {
+            logger.DidNotReceive().Information(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Log, entry).Serialize());
+        }
+
+        public static void should_not_have_logged_as_warning(this ILogger logger, object entry)
+        {
+            logger.DidNotReceive().Warning(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Log, entry).Serialize());
+        }
+
+        public static void should_not_have_logged_as_error(this ILogger logger, object entry)
+        {
+            logger.DidNotReceive().Error(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Log, entry).Serialize());
+        }
+
+        public static void should_not_have_logged_as_fatal(this ILogger logger, object entry)
+        {
+            logger.DidNotReceive().Fatal(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Log, entry).Serialize());
+        }
+
+        public static void should_not_have_logged_as_information<T>(this ILogger logger, Alert<T> entry)
+        {
+            logger.DidNotReceive().Information(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Alert, entry).Serialize());
+        }
+
+        public static void should_not_have_logged_as_warning<T>(this ILogger logger, Alert<T> entry)
+        {
+            logger.DidNotReceive().Warning(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Alert, entry).Serialize());
+        }
+
+        public static void should_not_have_logged_as_error<T>(this ILogger logger, Alert<T> entry)
+        {
+            logger.DidNotReceive().Error(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Alert, entry).Serialize());
+        }
+
+        public static void should_not_have_logged_as_fatal<T>(this ILogger logger, Alert<T> entry)
+        {
+            logger.Received().Fatal(new LogEntry<object>(Logging.Monitor, LogEntryTypes.Alert, entry).Serialize());
+        }
     }
 }
